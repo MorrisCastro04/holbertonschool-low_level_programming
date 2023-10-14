63ef4d9ad664fd99af9944e1730a8c2b59f20054
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-14 10:27:45 -0400

    first print

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 /**
  * main: is the starting point for the program execute
  * return: returns 0 when the program finish
  */
-
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
