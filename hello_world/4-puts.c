6e37ddd96f1a0ff0d4bfba8b516e7e5b37269469
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-14 10:08:16 -0400

    first print

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#main is a special type funcion and is the start of the program, where the executions starts.
 
-int main(void) {
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	getchar();
 	return (0);
