ef17b195f854d22b2e51e36093b0bdf3ac3887b8
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-14 20:17:25 -0400

    code to prinf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - return whats is in printf
- *printf - will print "print with proper grammar, but the outcome is a piece of art,"
- *return - 0 at the end
+ *main - Entry point
+ *printf: print "print with proper grammar, but the outcome is a piece of art,"
+ *return: Always return 0
  */
 int main(void)
 {
