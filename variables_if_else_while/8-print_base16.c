10b39ab23986a9c8ed2ef3d45844b65f3dc08190
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-16 15:02:02 -0400

    print hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	int n;
+	char a_f;
+
+	for (n = '0'; n <= '15'; n++)
+	{
+		putchar(n);
+	} for (a_f = 'a'; a_f <= 'f'; a_f++)
+	{ putchar(a_z);
+	} putchar('\n');
+	return (0);
+}
