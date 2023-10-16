4f16fa55f121d2f853d65a7faf51a1ba972e267e
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-16 10:34:51 -0400

    greater or less

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,11 +5,10 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+	int n, last_number;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_number;
 
 	last_number = n % 10;
 
