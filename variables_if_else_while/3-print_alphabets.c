4f6a1e4b969fdd6d965259a458b61e0af957ee6d
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-16 11:27:32 -0400

    the alphabet in lowercase and uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	char a_z, A_Z;
+
+	for (a_z = 'a'; a_z <= 'z'; a_z++)
+	{ putchar(a_z);
+	} for (A_Z = 'A'; A_Z <= 'Z'; A_Z++)
+	{ putchar(A_Z);
+	} putchar('\n');
+	return(0);
+}
