58f321deb09514beeeb4841e15f59344b2a097d9
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-16 13:36:16 -0400

    dont print q and e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,12 +7,13 @@
 int main(void)
 {
 	char a_z;
+
 	for (a_z = 'a'; a_z <= 'z'; a_z++)
 	{
 		if (a_z != 'q' && a_z != 'e')
 		{ putchar(a_z);
 		}
 	} putchar ('\n');
-	return(0);
+	return (0);
 }
 
