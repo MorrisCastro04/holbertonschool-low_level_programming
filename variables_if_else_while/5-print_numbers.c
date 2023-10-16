550267cfb20a432c774065e301d72ba5f727361c
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-16 14:00:49 -0400

    prirnt numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- *
- *
- *
+ *main - starts the program
+ *description: print 1 to 9
+ *Return: Return 0 always
  *
  */
 
 int main(void)
 { int n;
 
-	for( n = 0; n <= 9; n++)
+	for (n = 0; n <= 9; n++)
 	{ printf("%d", n);
 	}
 	putchar('\n');
