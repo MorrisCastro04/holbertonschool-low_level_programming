505149c81539b7a0447952ed13ab72fbb1f6233e
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-17 08:50:26 -0400

    count from 1 to 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - starts the program
+ *description: print from 1 to 10 
+ *Return: Return 0 always
+ */
+
+int main(void)
+{ int n;
+
+	for (n = 0; n <= 9; n++)
+	{ putchar(n);
+	  putchar(',');
+	  putchar(' ');
+	} putchar('\n');
+	return (0);
+}
