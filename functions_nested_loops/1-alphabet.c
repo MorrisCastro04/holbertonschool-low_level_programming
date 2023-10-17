a828aa37c7684e5e8d4d35cbed0b76190b9176d7
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-17 12:48:57 -0400

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+	char a;
+	int b;
+
+	print_alphabet(void)
+	{
+		for (a = 'a' ; a = 'z' ; a++)
+		{ putchar(a);
+		}
+	}
+}
+
