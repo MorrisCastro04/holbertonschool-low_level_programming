f96e38a7d95afb2d658391e7284b3c675b68071e
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-17 09:25:09 -0400

    count from 1 to 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,16 +6,18 @@
  */
 
 int main(void)
-{ int n;
+{
+	int n;
 
 	for (n = '0'; n <= '9'; n++)
 	{
 	putchar(n);
 	if (n != '9')
-		{
-	  putchar(',');
-	  putchar(' ');
-		}
-	} putchar('\n');
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
 	return (0);
 }
