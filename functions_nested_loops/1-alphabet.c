5daa821ff99031e978db29f758fd4288e87dd8e0
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-17 13:32:24 -0400

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,16 +7,16 @@
  *
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
+{ for (char a_z = 'a' ; a_z <= 'z' ; a_z++)
+	{ putchar(a_z);
+	}
+}
 
 int main(void)
 {
-	char a;
-	int b;
-
-	for (a = 'a' ; a <= 'z' ; a++)
-	{
-		putchar(a);
-	}
+	print_alphabet();
+	putchar('\n');
+	return (0);
 }
 
