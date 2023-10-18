b3a9251582292aea284d6939e936b58f0d945b75
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-17 22:04:11 -0400

    print alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int n = 0;
+
+	while (n < 10)
+	{
+		while (c < 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}
