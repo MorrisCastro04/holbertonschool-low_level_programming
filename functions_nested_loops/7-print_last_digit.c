beed92922c3d267cbd8f8c2ee5c8281ab8161bb9
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-19 09:21:42 -0400

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,14 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = (n % 10);
+	if (n < 0)
+	{
+		last_digit = (n * -1);
+		last_digit = (last_digit % 10);
+	}
+	else
+	{
+		last_digit = (n % 10);
+	}
 	return(last_digit);
 }
