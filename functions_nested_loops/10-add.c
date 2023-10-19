842ad666243cbe0a06d24bf4a8e6e29e6cabe7a0
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-19 13:31:00 -0400

    sum two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *add - sum two integers
+ *Return: Return the result of the sum
+ *
+ *
+ */
+
+int add(int one, int two)
+{
+	int result;
+
+	result = (one + two);
+	return (result);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -8,3 +8,4 @@ int _isalpha(int c);
 int _islower(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int add(int, int);
