0f2d786e3fbafc18c7dc9d5941eae2cc3693e078
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-19 10:23:01 -0400

    24hr clock

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ void jack_bauer(void)
 		for (min = 00 ; min <= 60 ; min++)
 		{
 
-			_putchar ((hr / 10 + '0');
+			_putchar ((hr / 10) + '0');
 			_putchar (':');
 			_putchar (min + '0');
 			_putchar (min + '0');
