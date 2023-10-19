7b5d1c1dc83e5fd6aad97884a4f36ebcde4c8731
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-19 14:07:14 -0400

    print natural numbers

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,12 +7,13 @@
 
 void print_to_98(int n)
 {
+	int i;
+
 	if (n < 98)
 	{
-		while(n <= 98)
+		for (i = n ; i <= 98 ; i++)
 		{
-			_putchar((n / 10) + '0');
-			n++;
+			printf("%d",n);
 		}
 	}
 	else
