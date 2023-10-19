6c454e8d8efe8e0275b10a43629d2641db0d3e09
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-19 07:15:42 -0400

    4 betty doc

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -12,12 +12,15 @@ int _isalpha(int c)
 {
 	int i;
 
-	for (i = 65 ; i <= 122 ; i++)
+	for (i = 65 ; i <= 90 ; i++)
 	{
 		if (c == i)
-		{
 			return (1);
-		}
+	}
+	for (i = 97 ; i<= 122 ; i++)
+	{
+		if (c == i)
+			return (1);
 	}
 	return (0);
 }
