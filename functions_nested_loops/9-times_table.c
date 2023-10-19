69c5620c0ae12903478237ae1d7e67b76182044e
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-19 12:48:36 -0400

    9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,12 +9,12 @@
 
 void times_table(void)
 {
-	int n, m, lines;
+	int n, m;
 
 	for ( n = 0 ; n <= 9 ; n++)
 	{
 		_putchar('0');
-		for (m = 1 ; m = <= 9 ; m++)
+		for (m = 1 ; m <= 9 ; m++)
 		{
 			_putchar(',');
 			_putchar(' ');
