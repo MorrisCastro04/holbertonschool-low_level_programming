fc65256ac4ed928fa95c20263eca879570344ee2
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-19 13:44:25 -0400

    print natural numbers

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_to_98 - print all natural numbers up to 98
+ *n@: is where the count starts
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		_putchar(n + '0');
+		n++;
+	}
+	else
+	{
+		_putchar(n + '0');
+		n--;
+	}
+}
