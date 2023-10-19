a815b618a781a696aae5fcd48fb1864ef4324c54
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-19 13:32:38 -0400

    sum two integers

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,8 +3,8 @@
 /**
  *add - sum two integers
  *Return: Return the result of the sum
- *
- *
+ *@one: is the first integer to sum
+ *@two: is the second integer to sum
  */
 
 int add(int one, int two)
