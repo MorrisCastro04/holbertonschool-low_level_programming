845fb273ba401b4b4bd08d7a46832839c4f44327
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-19 07:12:01 -0400

    4 betty doc

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -4,8 +4,8 @@
  *_isalpha - it will check if is lowercase ot uppercase
  *return : return 1 if the letter is lower case
  *description: this program check the cases
- *return: return 0 and 1
- *@c: is the lleter that the program will chack
+ *return: return 0,1
+ *@c: is the leter that the program will chack
  */
 
 int _isalpha(int c)
