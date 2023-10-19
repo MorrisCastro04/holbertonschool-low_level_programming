0731859ed5a49c7c859b34b33a52e46fd9d087d7
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-19 07:06:44 -0400

    absolute value

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *_abs - 
- *
- *
- *
- *
+ *_abs - runs the program
+ *@n: is the number to get is absolute value
+ *Return: Return @n if is positive and @-n if is negative
  */
 
 int _abs(int n)
