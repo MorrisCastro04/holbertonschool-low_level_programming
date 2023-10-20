72423a191562db0f62e23638d61657fd32d75a4d
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-20 16:21:48 -0400

    look for the upper letter

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ *
+ *
+ *
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 65 ; i <= 90 ; i++)
+	{
+		if (c == 1)
+		{
+			return (1);
+		} else
+		{
+			retunr (0);
+		}
+	}
+}
