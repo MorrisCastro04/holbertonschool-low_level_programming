a2861f3e0a14ef7e4174e886bd1446453502eb9e
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-21 10:18:59 -0400

    print 0 up to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	while (i = 0 ; i <= 9 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
