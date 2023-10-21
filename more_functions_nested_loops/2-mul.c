246f6b30cd448304cd68e11d68ffaed8f869c3ab
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-20 23:51:20 -0400

    program to multiplicate

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *mul - program to multiplicate
+ *@a: first digit of the mul
+ *@b: second digit of the mul
+ *Return: Return the result
+ */
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = (a * b);
+	return (m);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 int _putchar(char c);
+int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
 void times_table(void);
