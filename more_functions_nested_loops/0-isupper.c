df4e05a206428ec42a18446150abae6af1520ba2
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-20 23:14:25 -0400

    look for the upper letter

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *
- *
- *
- *
+ *_isupper - check if the letter is uppercase
+ *@c: is the letter that the programs check
+ *Return: Return 1 if upeercase if not return 0
  */
 
 int _isupper(int c)
