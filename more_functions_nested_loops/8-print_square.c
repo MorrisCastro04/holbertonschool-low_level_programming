6d8e1d5e05c1d727dfb48074de8a18feeec9105e
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-22 01:26:15 -0400

    print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_square - prints a square of #
+ *@size: it will be the size of the square
+ */
+
+void print_square(int size)
+{
+	int row, column;
+
+	if (size >= 1)
+	{
+		for (column = 0; column <= size; column++)
+		{
+			for (row = 0; row <= size; row++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 int _putchar(char c);
+void print_square(int size);
 void print_diagonal(int n);
 void print_line(int n);
 void print_most_numbers(void);
