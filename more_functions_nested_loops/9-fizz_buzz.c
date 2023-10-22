65e63ff7e510d61298465391f649df250b616c65
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-22 13:00:38 -0400

    FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,29 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (mult1 == num && mult2 == num)
+		if (mult1 == num || mult2 == num)
 		{
-			printf("FizzBuzz");
-			mult1 = (mult1 + 3);
-			mult2 = (mult2 + 5);
+			if (mult1 == num && mult2 == num)
+			{
+				printf("FizzBuzz");
+				mult1 = (mult1 + 3);
+				mult2 = (mult2 + 5);
+			}
+			else if (mult1 == num)
+			{
+				printf("Fizz");
+				mult1 = (mult1 + 3);
+			}
+			else if (mult2 == num)
+			{
+				printf("Buzz");
+				mult2 = (mult2 + 5);
+			}
 		}
-		else if (mult1 == num)
+		else
 		{
-			printf("Fizz");
-			mult1 = (mult1 + 3);
+			printf("%d", num);
 		}
-		else if (mult2 == num)
-		{
-			printf("Buzz");
-			mult2 = (mult2 + 5);
-		}
-		printf("%d", num);
 		printf(" ");
 	}
 	return(0);
