ff2ab67c7e0eaa9f2c2529985e33e1a52aba71f6
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-22 20:53:29 -0400

    create triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,17 +11,17 @@ void print_triangle(int size)
 {
 	int lines, trg;
 
-	if size <= 0)
+	if (size <= 0)
 	{
-		_putchar("\n");
+		_putchar('\n');
 	}
 	for (lines = 0; lines <= size; lines++)
 	{
 		for (trg = 0; trg <= size; trg++)
 		{
-			_putchar("\\")
+			_putchar('\\');
 		}
-		_putchar("\n");
+		_putchar('\n');
 	}
 
 }
