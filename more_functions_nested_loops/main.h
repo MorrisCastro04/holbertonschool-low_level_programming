8105709011b37dab47a1e1bb8f293df59f3fa51d
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-23 09:30:06 -0400

    more numbers

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@ int _putchar(char c);
 void print_triangle(int size);
 void print_square(int size);
 void print_diagonal(int n);
+void more_numbers(void);
 void print_line(int n);
 void print_most_numbers(void);
 void print_numbers(void);
