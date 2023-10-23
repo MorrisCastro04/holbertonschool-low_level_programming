3c1f2cad7f5ab883706eb935ef1bea4214af1e30
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-23 13:24:19 -0400

    swap var values

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *swap_int - swap the value of 2 var
+ *@a: the first var to swap
+ *@b: the second var to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
