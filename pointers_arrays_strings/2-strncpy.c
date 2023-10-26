1a2a7d5efc358cb0f699c7e73372d66f0478a287
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-26 18:15:05 -0400

    copy the string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strncpy - copy a string
+ *@dest: is where in going to paste the string
+ *@src: is the string that is going to copy
+ *Return: Return the new string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int lengh, i;
+
+	lengh = 0;
+	while (dest[lengh] != '\0')
+	{
+		for (i = 0; i <= n; i++;)
+		{
+			dest[i] = src[i];
+		}
+		lengh++;
+	}
+	dest[lengh] = '\0';
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 int _putchar(char c);
+char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 int _atoi(char *s);
