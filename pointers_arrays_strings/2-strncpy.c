2ca7a19c5e0ea3a9b8168858727705be1eae209e
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-26 18:54:16 -0400

    copy the string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,18 +4,21 @@
  *@dest: is where in going to paste the string
  *@src: is the string that is going to copy
  *Return: Return the new string
+ *@n: the s[ace of bytes
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, lengh;
 
-	while(dest[i] != '\0')
+	lengh = 0;
+	while(dest[lengh] != '\0')
 	{
 		for (i = 0; i < n && src[i] != '\0'; i++)
 		{
 			dest[i] = src[i];
 		}
+		lengh++;
 	}
 	dest[i] = '\0';
 	return (dest);
