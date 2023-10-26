f5c00e90490482e5a833f5da0ce9701a478f0733
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-26 13:54:14 -0400

    concatenates strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ *_strcat = concatenate 2 strings
+ *@dest: where the 2 string need to be
+ *@src: the string that is going to move
+ *Return: Return the full string
  */
 
 char *_strcat(char *dest, char *src)
@@ -11,7 +11,7 @@ char *_strcat(char *dest, char *src)
 	int i, lengh;
 
 	lengh = 0;
-	i = 0i;
+	i = 0;
 	while (dest[lengh] != '\0')
 	{
 		lengh++;
