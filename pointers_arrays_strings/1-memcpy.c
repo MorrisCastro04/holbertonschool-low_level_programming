a09913e07734b2bbfb6e5c31ba5a85e3654fb2fb
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-28 18:54:23 -0400

    copies memory

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_memcpy - copies memory area
+ *@dest: Where im pasting the memory
+ *@src: where im coping the memory
+ *@n: nomber of byte of memory to copy
+ *Return: Return the new array
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int lengh;
+
+	for (lengh = 0; lengh < n; lengh++)
+	{
+		dest[lengh] = src[lengh];
+	}
+	return (dest);
+}
