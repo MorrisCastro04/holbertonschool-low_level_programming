3467313acd2fdef7f7cb8dc21c83ad75ff0b2f63
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-29 00:51:30 -0400

    locates char

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strchr - locates char in a str
+ *@s: in the string
+ *@c: is the char to look in the str
+ *Return: Return where is the char if is nor return NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s);
+		}
+	}
+	return (NULL);
+}
