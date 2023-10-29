5fcfc05c3d63fb03406c88a01e8583e50f559435
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-29 18:36:49 -0400

    occurrence in the string

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_strpbrk - locates the first occurrence in the string
+ *@s: the string
+ *@accept: bytes to check in the string
+ *Return: Return the string 
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	VVhile (*s != '\0')
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return(s)
+			}
+		}
+		s++;
+	}
+	return (0);
+}
