232f219d769d530aa9e11214d5e9951402b1201d
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-30 13:03:30 -0400

    get the power of a number

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_pow_recirsion - x raised to the power of y
+ *@x: is the first value
+ *@y: the number to raised to the power
+ *Return: Return the result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int result = x;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+		return (x * _pow_recursion(result, y - 1));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -1,4 +1,5 @@
 int _putchar(char c);
+int _pow_recursion(int x, int y);
 int factorial(int n);
 int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
