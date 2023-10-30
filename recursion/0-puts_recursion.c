5022373f3ab40e1846af1895e3f3d0ce3ccd9ca4
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-30 11:25:37 -0400

    print string reverse

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- *_putchar_recursion - print a string
+ *_puts_recursion - print a string
  *@s: the string to print
  */
 
 void _puts_recursion(char *s)
 {
+
 	if (*s != '\0')
 	{
 		_putchar(*s);
