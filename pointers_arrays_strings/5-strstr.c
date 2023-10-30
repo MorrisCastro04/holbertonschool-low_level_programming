09904219cf647578ade781203af63342435d66a2
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-29 23:07:42 -0400

    locates substrings

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,7 +11,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (lengh = 0; needle[lengh] != '\0'; j++)
+		for (lengh = 0; needle[lengh] != '\0'; lengh++)
 		{
 			if (haystack[i + lengh] != needle[lengh])
 				break;
