25bb67273b7467162ed16793f535ac91450759a1
MorrisCastro <jomarcastro2004@gmail.com>
2023-10-31 11:02:04 -0400

    print the file name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -9,14 +9,9 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
+	if (argc == 0)
 	{
-		if (argc == 0)
-		{
-			printf("%s\n", argv[0]);
-		}
+		printf("%s\n", argv[0]);
 	}
-		return (0);
+	return (0);
 }
