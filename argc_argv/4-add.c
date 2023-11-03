17846ab4d01be6bead8532923e1ba07c1cf1b968
MorrisCastro <jomarcastro2004@gmail.com>
2023-11-03 10:38:08 -0400

    sum numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,17 +12,19 @@ int main(int argc, char *argv[])
 	int i, sum;
 
 	sum = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < '0' || *argv[i] > '9' || sum == 199)
+		if (!isdigit(atoi(argv[i])))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		else
-		{
-			sum += atoi(argv[i]);
-		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
