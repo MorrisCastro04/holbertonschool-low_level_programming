ea78b1ff07b736315495612839c426ebac29c3e0
MorrisCastro <jomarcastro2004@gmail.com>
2023-11-08 10:29:13 -0400

    allocate memory

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *malloc_checked - allocate memory using malloc
+ *@b: Size of the space in memory
+ *Return: Return a pointer to the malloc
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *mall;
+
+	if (b == 0)
+	{
+		exit(98);
+	}
+	mall = malloc(sizeof(int) * b);
+
+	if (mall == NULL)
+	{
+		exit(98);
+	}
+	return (mall);
+}
