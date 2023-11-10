df414908273daa0dc50eb268f3be47d76836b46d
MorrisCastro <jomarcastro2004@gmail.com>
2023-11-09 21:26:35 -0400

    aaray of integer

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *array_range - array of integers
+ *@min: the first number
+ *@max: the last number
+ *Return: Return a pointer to the string
+ */
+
+int *array_range(int min, int max)
+{
+	int i, range;
+	int *mall;
+
+	range = max - min;
+	if (min > max)
+	{
+		return (NULL);
+	}
+	mall = malloc(sizeof(int) * range);
+	if (mall == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < range; i++)
+	{
+		mall[i] = min;
+		min++;
+	}
+	return (mall);
+}
