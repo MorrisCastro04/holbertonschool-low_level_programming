ac39320b9a16b64e8ba456b35743dff54b1fa79f
MorrisCastro <jomarcastro2004@gmail.com>
2023-11-14 13:56:01 -0400

    new type dog_t

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -3,40 +3,35 @@
 /**
  *print_dog - funcion to print struct dog
  *@d: pointer to struct dog
- *
  */
 
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name == NULL && d->age < 0)
+		if (d->name != NULL)
 		{
-			printf("Name: (nil)\nAge: (nil)\nOwner: %s\n", d->owner);
-		}
-		else if (d->name == NULL && d->owner == NULL)
-		{
-			printf("Name: (nil)\nAge: %f\nOwner: (nil)\n", d->age);
-		}
-		else if (d->age < 0 && d->owner == NULL)
-		{
-			printf("Name: %s\nAge: (nil)\nOwner: (nil)\n", d->name);
-		}
-		else if (d->name == NULL)
-		{
-			printf("Name: (nil)\nAge: %f\nOwner: %s\n", d->age, d->owner);
-		}
-		else if (d->age < 0)
-		{
-			printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
-		}
-		else if (d->owner == NULL)
-		{
-			printf("Name: %s\nAge: %f\nOwner: (nil)\n", d->name, d->age);
+			printf("Name: %s\n", d->name);
 		}
 		else
 		{
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+			printf("Name: (nil)\n");
+		}
+		if (d->age > 0)
+		{
+			printf("Age: %f\n", d->age);
+		}
+		else
+		{
+			printf("Age: (nil)\n");
+		}
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
 		}
 	}
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,13 +6,15 @@
  *@age: age of the dog
  *@owner: owner of the dog
  */
+
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
-#endif
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
+#endif
