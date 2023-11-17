de7b7666d5ededeb54511691404171997cfb02c7
MorrisCastro <jomarcastro2004@gmail.com>
2023-11-17 10:03:45 -0400

    repair main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 /**
  *
@@ -14,12 +15,23 @@ int main (int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (0);
+		exit(98);
 	}
-
+	if (!(strcmp(argv[2], "+") == 0 || strcmp(argv[2], "-") == 0 ||
+		strcmp(argv[2], "*") == 0 || strcmp(argv[2], "/") == 0 ||
+		strcmp(argv[2], "%") == 0))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[1] == 0 || argv[3] == 0) && (strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0))
+	{
+		printf("Error");
+		exit(100);
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	result = get_op_func(argv[2])(num1, num2);
-	printf ("%d", result);
+	printf ("%d\n", result);
 	return (0);
 }
