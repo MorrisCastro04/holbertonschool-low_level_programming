b80046fa276bfe9b6f8dc4f7ae3701904ffa5b19
MorrisCastro <jomarcastro2004@gmail.com>
2023-11-19 10:35:12 -0400

    write the betty

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,9 +2,9 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- *
- *
- *
+ *print_numbers - funcion to print numbers
+ *@separator: char thats go between numbers
+ *@n: how many numbers is gonna print
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -12,10 +12,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int i;
 
-	va_start (list, n);
+	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
 		int num = va_arg(list, int);
+
 		printf("%d", num);
 		if ((i + 1) != n && separator != NULL)
 		{
