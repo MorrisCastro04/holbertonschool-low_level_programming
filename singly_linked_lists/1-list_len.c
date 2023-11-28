addf3e4816974d722a30c3fdd5568e94a5fd6730
MorrisCastro <jomarcastro2004@gmail.com>
2023-11-28 12:43:44 -0400

    restard the code and use a while

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -9,9 +9,10 @@ size_t list_len(const list_t *h)
 {
     size_t elem = 0;
 
-    if (h->len > 0)
+    while (h != NULL)
+    {
         elem++;
-    if (h->str != NULL)
-        elem++;
+        h = h->next;
+    }
     return (elem);
 }
