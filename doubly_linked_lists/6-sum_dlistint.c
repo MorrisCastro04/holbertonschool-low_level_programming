c422cb80381920de835090bb62aea630f46a20f7
MorrisCastro <jomarcastro2004@gmail.com>
2023-12-01 09:41:58 -0400

    sum all the data

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sum all the data of the list
+ * @head: pointer to the link list
+ * Return: Return the sum of all the nodes
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	int count = 0;
+
+	while (temp != NULL)
+	{
+		count += temp->n;
+		temp = temp->next;
+	}
+    return (count);
+}
