84ff4ddb8cf11bddb4329db9c257e713ffc87235
MorrisCastro <jomarcastro2004@gmail.com>
2023-12-03 10:39:43 -0400

    fix the tabs for betty

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,41 +10,41 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *newNode, *prevNode, *tmp = *h;
-    unsigned int size = 0;
+	dlistint_t *newNode, *prevNode, *tmp = *h;
+	unsigned int size = 0;
 
-    newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
-    if (newNode == NULL)
-        return (NULL);
-    if (h == NULL)
-        return (NULL);
-    while (tmp != NULL)
-    {
-        tmp = tmp->next;
-        size++;
-    }
-    if (idx > size)
-        return (NULL);
-    newNode->n = n;
-    if (idx == 0)
-    {
-        newNode->prev = NULL;
-        newNode->next = *h;
-        if (*h != NULL)
-            (*h)->prev = newNode;
-        *h = newNode;
-        return (newNode);
-    }
-    tmp = *h;
-    while (--idx)
-    {
-        prevNode = tmp;
-        tmp = tmp->next;
-    }
-    newNode->next = tmp;
-    newNode->prev = prevNode;
-    prevNode->next = newNode;
-    if (tmp != NULL)
-        tmp->prev = newNode;
-    return (newNode);
+	newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+	if (h == NULL)
+		return (NULL);
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		size++;
+	}
+	if (idx > size)
+		return (NULL);
+	newNode->n = n;
+	if (idx == 0)
+	{
+		newNode->prev = NULL;
+		newNode->next = *h;
+		if (*h != NULL)
+			(*h)->prev = newNode;
+		*h = newNode;
+		return (newNode);
+	}
+	tmp = *h;
+	while (--idx)
+	{
+		prevNode = tmp;
+		tmp = tmp->next;
+	}
+	newNode->next = tmp;
+	newNode->prev = prevNode;
+	prevNode->next = newNode;
+	if (tmp != NULL)
+		tmp->prev = newNode;
+	return (newNode);
 }
