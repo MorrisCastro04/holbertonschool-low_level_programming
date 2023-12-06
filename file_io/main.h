fbf01498d053f0e632d6cfff97fc8157f74319b4
MorrisCastro <jomarcastro2004@gmail.com>
2023-12-06 18:55:14 -0400

    fixing betty at the main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef main_H
 #define main_H
+
 #include <stdlib.h>
 #include <stddef.h>
 #include <unistd.h>
@@ -11,4 +12,5 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 #endif
