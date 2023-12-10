d85b290011219738f5c79f27d238e2c705bd7b53
MorrisCastro <jomarcastro2004@gmail.com>
2023-12-10 13:30:40 -0400

    fix tabs

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -7,19 +7,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int i;
-    hash_node_t *tmp;
+	unsigned long int i;
+	hash_node_t *tmp;
 
-    if (ht == NULL || key == NULL)
-        return (0);
+	if (ht == NULL || key == NULL)
+		return (0);
 
-    i = key_index((unsigned char *)key, ht->size);
-    tmp = ht->array[i];
-    while (tmp)
-    {
-        if (strcmp(tmp->key, key) == 0)
-            return (tmp->value);
-        tmp = tmp->next;
-    }
-    return (NULL);
+	i = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[i];
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
