9b1853ae6e03d33d21805e6ce33bc7e2b52f6584
MorrisCastro <jomarcastro2004@gmail.com>
2023-12-10 13:28:00 -0400

    value associated to the key

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - gets value of the key
+ * @ht: the hash table
+ * @key: the key
+ * Return: Return the value otherwise NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t *tmp;
+
+	if (!ht == NULL || key == NULL)
+		return (0);
+
+	i = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[i];
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
