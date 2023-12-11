0db7f0e83eaa66fc3bc3665c8e87228fdcf8c340
MorrisCastro <jomarcastro2004@gmail.com>
2023-12-10 20:28:53 -0400

    print the hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,46 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print a hash table
+ * @ht: a pointer to the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, j = 0;
+	hash_table_t *table_tmp;
+	hash_node_t *node_tmp;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			table_tmp->array[j] = ht->array[i];
+			j++;
+		}
+	}
+	for (i = 0; i < j; i++)
+	{
+		node_tmp = table_tmp->array[i];
+		if (i == 0)
+			printf("{");
+		if (table_tmp->array[i]->next == NULL)
+		{
+			printf("'%s: '%s", node_tmp->key, node_tmp->value);
+			if (i < j - 1)
+				printf(", ");
+			else
+				printf("}\n");
+		}
+		if (table_tmp->array[i]->next != NULL)
+		{
+			while (node_tmp)
+			{
+				printf("'%s: '%s", node_tmp->key, node_tmp->value);
+				printf(", ");
+				node_tmp = node_tmp->next;
+			}
+		}
+	}
+}
